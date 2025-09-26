8a0880399e7badd4034a628f8fe2cd101834d089
KONJETI GNANA VENKATA NAGA GOPI PRASANTH <2300031235@kluniversity.in>
2025-09-26 20:31:43 +0530

    Update Login.jsx

--- Portfolio/src/COMPONENTS/Login.jsx
+++ Portfolio/src/COMPONENTS/Login.jsx
@@ -64,7 +64,7 @@ function Login() {
     <div className="page-wrapper">
       {/* Portfolio app in center */}
       <div className="portfolio-container">
-        <h1>Portfolio app</h1>
+        <h1>Portfolio App</h1>
       
       </div>
 
